7bba6b2b9e12cf099a102124c1c9519f53163a04
trevisharp <leonardosilio@alunos.utfpr.edu.br>
2023-11-30 09:06:46 -0300

    Startup with code moved from Orkestra repository

---
+++ src/Usings.cs
@@ -0,0 +1,6 @@
+global using System;
+global using Orkestra;
+global using Orkestra.LexicalAnalysis;
+global using Orkestra.SyntacticAnalysis;
+global using Orkestra.Processings;
+global using Orkestra.Errors;
